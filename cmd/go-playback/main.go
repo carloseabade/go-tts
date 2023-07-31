64bfd3fa79ac20be52a828af5ddfb36c6b321121
Carlos Abade <carloseabade@gmail.com>
2023-07-31 12:20:07 -0300

    feat: add go-playback

---
+++ cmd/go-playback/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "os/exec"
+
+	"github.com/carloseabade/go-tts/internal/flags"
+	"github.com/carloseabade/go-tts/internal/go_tts"
+	"github.com/carloseabade/go-tts/pkg/tts"
+)
+
+func main() {
+	flags.Parse()
+
+	switch {
+	case len(flags.Args()) > 0:
+		flags.NotACommand()
+	case flags.Help:
+		flags.Usage()
+	case flags.Text != "" || flags.File != "" || flags.ListVoices:
+		if flags.Text != "" {
+			go_tts.TTS(go_tts.TEXT, flags.Text, flags.WriteMedia, flags.Voice, flags.Rate, flags.Volume, flags.Proxy)
+      runMPV()
+		} else if flags.File != "" {
+			go_tts.TTS(go_tts.FILE, flags.File, flags.WriteMedia, flags.Voice, flags.Rate, flags.Volume, flags.Proxy)
+      runMPV()
+		} else {
+			tts.PrintVoices(flags.Proxy)
+		}
+	default:
+		flags.NoExecutableCommand()
+	}
+}
+
+func runMPV() {
+  cmd := exec.Command("mpv", flags.WriteMedia)
+  cmd.Stdout = os.Stdout
+
+  if err := cmd.Run(); err != nil {
+    fmt.Println("could not run command: ", err)
+  }
+}
