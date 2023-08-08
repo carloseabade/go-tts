bf241d75e8f117e46b73f1efb632d41c5469ed3a
Carlos Abade <carloseabade@gmail.com>
2023-08-07 23:22:45 -0300

    fix: remove comment lines

--- cmd/go-tts/main.go
+++ cmd/go-tts/main.go
@@ -24,10 +24,3 @@ func main() {
 		flags.NoExecutableCommand()
 	}
 }
-
-//edge-tts --text "Hello, world!" --write-media hello.mp3 --write-subtitles hello.vtt
-//edge-playback --text "Hello, world!"
-//edge-tts --list-voices
-//edge-tts --voice ar-EG-SalmaNeural --text "مرحبا كيف حالك؟" --write-media hello_in_arabic.mp3 --write-subtitles hello_in_arabic.vtt
-//edge-tts --rate=-50% --text "Hello, world!" --write-media hello_with_rate_halved.mp3 --write-subtitles hello_with_rate_halved.vtt
-//edge-tts --volume=-50% --text "Hello, world!" --write-media hello_with_volume_halved.mp3 --write-subtitles hello_with_volume_halved.vtt
